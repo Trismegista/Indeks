0744f20d173d1ac7d4be4491d455339fdac1e2cd
Pawel Kubacki <pawel.kubacki@outlook.com>
2013-06-06 06:05:37 +0200

    LogInWindow and Beggining of MainWindow

---
+++ Indeks/LogInView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Indeks
+{
+    /// <summary>
+    /// Interaction logic for LogInView.xaml
+    /// </summary>
+    public partial class LogInView : Window
+    {
+        public LogInView()
+        {
+            InitializeComponent();
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
---
+++ Indeks/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Indeks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DisplayLoginScreen();
+        }
+        private void DisplayLoginScreen()
+        {
+            LogInView frm = new LogInView();
+
+            frm.ShowDialog();
+            if (frm.DialogResult.HasValue && frm.DialogResult.Value)
+                MessageBox.Show("User Logged In");
+            else
+                this.Close();
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+            private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
