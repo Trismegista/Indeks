be88a666059a46ea064012cda08faf59fb173fde
Pawel Kubacki <pawel.kubacki@outlook.com>
2013-06-22 07:52:29 +0200

    asdasd

---
+++ Indeks/ViewModels/ProfileEditVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indeks.ViewModels
+{
+    public class ProfileEditVM : ApplicationVM
+    {
+        public ProfileEditVM()
+        {
+
+        }
+    }
+}
